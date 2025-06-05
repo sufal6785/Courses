177471f3b9f230bb4318c3d09533b95aa7055a4f
sufal6785 <sufal.legend.77@gmail.com>
2025-06-05 22:21:48 +0600

    Class Implement of List and Cpp class directoy are created

---
+++ Cpp-Class-Implement/Linked_List_Singly.cpp
@@ -0,0 +1,250 @@
+#include <iostream>
+using namespace std;
+
+class List
+{
+private:
+    // encapsulating node within list class to prventing external access & keeping it self contained
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+
+        Node(int val) : data(val), next(nullptr) {}
+    };
+    Node *head, *tail;
+    int size_n; // for calculating size without traversing the entire list
+
+public:
+    List() : head(nullptr), tail(nullptr), size_n(0) {}
+
+    ~List() // List destructor to deallocate memory to prevent memory leaked
+    {
+        Node *current = head;
+        while (current != nullptr)
+        {
+            Node *nextNode = current->next;
+            delete current;     // deleting current node
+            current = nextNode; // shifting towards the next node
+        }
+    }
+
+    void push_back(int);
+
+    void push_front(int);
+
+    void insert(int, int);
+
+    void pop_back();
+
+    void pop_front();
+
+    void pop(int);
+
+    int size();
+
+    void display();
+};
+
+void List::push_back(int data)
+{
+    Node *newNode = new Node(data); // allocating memory for  node
+    if (head == nullptr)            // empty list
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode; // creating link
+        tail = newNode;       // shifting tail to newnode
+    }
+    size_n++;
+}
+
+void List::push_front(int data)
+{
+    Node *newNode = new Node(data);
+    if (tail == nullptr) // if tail is null, pointig head and tail to newNode
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        newNode->next = head;
+        head = newNode;
+    }
+    size_n++;
+}
+
+void List::insert(int data, int position) // 0 based indexing
+{
+    if (position < 0 || position > size())
+    {
+        cout << "Invalid Index" << endl;
+        return;
+    }
+
+    if (position == 0)
+    {
+        push_front(data);
+        return;
+    }
+
+    if (position == size())
+    {
+        push_back(data);
+        return;
+    }
+
+    Node *current = head, *prev = head;
+    for (int i = 0; i < position; i++)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    Node *newNode = new Node(data);
+    prev->next = newNode;
+    newNode->next = current;
+
+    size_n++;
+}
+
+void List::pop_back()
+{
+    if (head == nullptr) // empty list
+    {
+        cout << "Empty List" << endl;
+        return;
+    }
+
+    if (head == tail) // single element
+    {
+        delete head;
+        head = tail = nullptr;
+    }
+    else
+    {
+        Node *current = head;
+        while (current->next != tail)
+        {
+            current = current->next;
+        }
+        current->next = nullptr; // marking the end of the list
+        delete tail;             // deleting last node
+        tail = current;          // updating tail to current
+    }
+
+    size_n--;
+}
+
+void List::pop_front()
+{
+    if (head == nullptr) // empty list
+    {
+        cout << "Empty list" << endl;
+        return;
+    }
+
+    if (head == tail) // single element
+    {
+        delete head;
+        head = tail = nullptr;
+    }
+    else
+    {
+        Node *current = head;
+        head = head->next;
+        delete current;
+    }
+
+    size_n--;
+}
+
+void List::pop(int key)
+{
+    if (!head)  //empty list
+    {
+        cout << "Empty List" << endl;
+        return;
+    }
+    if (head && head->data == key) // key is first element
+    {
+        pop_front();
+        return;
+    }
+
+    if (tail && tail->data == key) // key is last element
+    {
+        pop_back();
+        return;
+    }
+
+    Node *current = head;
+    Node *prev = head;
+    while (current && current->data != key) // to find the key node
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (!current) // when key not found
+    {
+        cout << "Key not found" << endl;
+        return;
+    }
+
+    Node *temp = current;       // to prevent memory leak
+    prev->next = current->next; // skip current Node
+    delete (temp);              // delet target node
+
+    size_n--;
+}
+
+int List::size()
+{
+    return size_n;
+}
+
+void List::display()
+{
+    if (!head)
+    {
+        cout << "Empty List" << endl;
+        return;
+    }
+
+    Node *current = head;
+    while (current)
+    {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    List lst;
+    // lst.push_back(1);
+    // lst.push_back(2);
+    // lst.push_back(3);
+    // lst.push_back(4);
+
+    // lst.push_front(77);
+
+    // lst.pop_front();
+
+    // lst.pop_back();
+
+    // lst.pop(77);
+
+    // lst.pop(1);
+
+    lst.pop(3);
+
+    cout << lst.size() << endl;
+
+    lst.display();
+}
---
+++ Cpp-Class-Implement/ff.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    list<int>lst;
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(3);
+
+    for(auto val: lst) cout<<val<<" ";
+    cout<<endl;
+}

f39a8d507acf82b09ab7907ae2074d67a2808f1a
sufal6785 <sufal.legend.77@gmail.com>
2025-06-11 16:06:32 +0600

    Template for GeeksForGeeks

---
+++ DSA/Linked_List/template.cpp
@@ -0,0 +1,20 @@
+// Given the head of a singly linked list, your task is to left rotate the linked list k times.
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    
+};
